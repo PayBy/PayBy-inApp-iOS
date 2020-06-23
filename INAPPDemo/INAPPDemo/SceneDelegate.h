207c5bdeb3d08d8cf9a36d6bde301b43ae55be67
fengbing <fengbing@g42pay.com>
2020-06-23 14:44:41 +0400

    update INAPP Demo framwork--- add

---
+++ INAPPDemo/INAPPDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  INAPPDemo
+//
+//  Created by ice on 2020/6/23.
+//  Copyright © 2020 ice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
